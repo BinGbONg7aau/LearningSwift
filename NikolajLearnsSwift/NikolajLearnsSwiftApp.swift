35c94366d22f2164849faa48d9e83ab212e8654f
Nikomiko <ngool23@student.aau.dk>
2026-07-02 21:31:11 +0200

    Initial commit

---
+++ NikolajLearnsSwift/NikolajLearnsSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  NikolajLearnsSwiftApp.swift
+//  NikolajLearnsSwift
+//
+//  Created by Nikolaj van Gool on 02/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct NikolajLearnsSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
