85f129941b4fc4c835d4cc9178114cb153aa86cb
Nikomiko <ngool23@student.aau.dk>
2026-07-05 22:26:13 +0200

    Tests and functions

---
+++ NikolajLearnsSwift/PlayerList/GameState.swift
@@ -0,0 +1,14 @@
+//
+//  GameState.swift
+//  NikolajLearnsSwift
+//
+//  Created by Nikolaj van Gool on 05/07/2026.
+//
+
+import Foundation
+
+enum GameState {
+    case setup
+    case playing
+    case gameOver
+}
--- NikolajLearnsSwift/PlayerList/PlayerListView.swift
+++ NikolajLearnsSwift/PlayerList/PlayerListView.swift
@@ -8,50 +8,89 @@
 import SwiftUI
 
 struct PlayerListView: View {
-    @State private var players: [Player] = [
-        Player(name: "Elisha", score: 0),
-        Player(name: "Andre", score: 0),
-        Player(name: "Jasmine", score: 0),
-    ]
+    @State private var scoreboard = Scoreboard()
+    @State private var startingPoints = 0
     
     var body: some View {
-            VStack(alignment: .leading) {
-                Text("Score Keeper")
-                    .font(.title)
-                    .bold()
-                    .padding(.bottom)
+        VStack(alignment: .leading) {
+            Text("Score Keeper")
+                .font(.title)
+                .bold()
+                .padding(.bottom)
 
 
-                Grid {
+            SettingsView(
+                doesHighestScoreWin: $scoreboard.doesHighestScoreWin,
+                startingPoints: $startingPoints)
+                .disabled(scoreboard.state != .setup)
+
+
+            Grid {
+                GridRow {
+                    Text("Player")
+                        .gridColumnAlignment(.leading)
+                    Text("Score")
+                        .opacity(scoreboard.state == .setup ? 0 : 1.0)
+                }
+                .font(.headline)
+
+
+                ForEach($scoreboard.players) { $player in
                     GridRow {
-                        Text("Player")
-                            .gridColumnAlignment(.leading)
-                        Text("Score")
-                    }
-                    .font(.headline)
-
-
-                    ForEach($players) { $player in
-                        GridRow {
+                        HStack {
+                            if scoreboard.winners.contains(player) {
+                                Image(systemName: "crown.fill")
+                                    .foregroundStyle(Color.yellow)
+                            }
                             TextField("Name", text: $player.name)
-                            Text("\(player.score)")
-                            Stepper("\(player.score)", value: $player.score)
-                                .labelsHidden()
+                                .disabled(scoreboard.state != .setup)
                         }
+                        Text("\(player.score)")
+                            .opacity(scoreboard.state == .setup ? 0 : 1.0)
+                        Stepper("\(player.score)", value: $player.score)
+                            .labelsHidden()
+                            .opacity(scoreboard.state == .setup ? 0 : 1.0)
                     }
                 }
-                .padding(.vertical)
+            }
+            .padding(.vertical)
 
 
-                Button("Add Player", systemImage: "plus") {
-                    players.append(Player(name: "", score: 0))
+            Button("Add Player", systemImage: "plus") {
+                scoreboard.players.append(Player(name: "", score: 0))
+            }
+            .opacity(scoreboard.state == .setup ? 1.0 : 0)
+
+
+            Spacer()
+
+
+            HStack {
+                Spacer()
+                switch scoreboard.state {
+                case .setup:
+                    Button("Start Game", systemImage: "play.fill") {
+                        scoreboard.state = .playing
+                        scoreboard.resetScores(to: startingPoints)
+                    }
+                case .playing:
+                    Button("End Game", systemImage: "stop.fill") {
+                        scoreboard.state = .gameOver
+                    }
+                case .gameOver:
+                    Button("Reset Game", systemImage: "arrow.counterclockwise") {
+                        scoreboard.state = .setup
+                    }
                 }
-
-
                 Spacer()
             }
-            .padding()
+            .buttonStyle(.bordered)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
+            .tint(.blue)
         }
+        .padding()
+    }
 }
 
 #Preview {
